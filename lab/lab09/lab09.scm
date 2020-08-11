48ddc89e7824c15aff661bbfc8cdf2385ed9abf7
ChenShangheng <3500161@qq.com>
2020-08-12 07:59:00 +0800

    Lab09

---
+++ lab/lab09/lab09.scm
@@ -0,0 +1,73 @@
+;; Scheme ;;
+
+(define (over-or-under x y)
+  (if (< x y) -1 (if (= x y) 0 1))
+)
+
+;;; Tests
+(over-or-under 1 2)
+; expect -1
+(over-or-under 2 1)
+; expect 1
+(over-or-under 1 1)
+; expect 0
+
+(define (filter-lst f lst)
+  (if (null? lst) lst
+    (if (f (car lst))
+    	(cons (car lst) (filter-lst f (cdr lst)))
+    	(filter-lst f (cdr lst))
+    )
+  )  
+)
+
+;;; Tests
+(define (even? x)
+  (= (modulo x 2) 0))
+(filter-lst even? '(0 1 1 2 3 5 8))
+; expect (0 2 8)
+
+(define (make-adder num)
+  (lambda (x) (+ num x))
+)
+
+;;; Tests
+(define adder (make-adder 5))
+(adder 8)
+; expect 13
+
+;; Extra questions
+
+(define lst
+  (cons (list 1) (cons 2 (cons (list 3 4) (list 5))))
+)
+
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
+
+(define (remove item lst)
+  (filter-lst (lambda (x) (not (= x item)))lst)
+)
+
+
+;;; Tests
+(remove 3 nil)
+; expect ()
+(remove 3 '(1 3 5))
+; expect (1 5)
+(remove 5 '(5 3 5 5 1 4 5 4))
+; expect (3 1 4 4)
+
+(define (no-repeats s)
+  'YOUR-CODE-HERE
+)
+
+(define (substitute s old new)
+  'YOUR-CODE-HERE
+)
+
+
+(define (sub-all s olds news)
+  'YOUR-CODE-HERE
+)
